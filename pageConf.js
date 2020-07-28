125152ac6119c8e09713cc8238d69375df3ad519
zhuwenfang <zhuwenfang@inspur.com>
2020-07-28 15:43:34 +0800

    自动生成单页面

---
+++ pageConf.js
@@ -0,0 +1,24 @@
+
+/**
+ * 自动生成文件的相关配置信息
+ * @param parentFolderPath 父文件地址
+ * @param author 当前文件的创建者
+ * @param title 文件标题，与增删改的提示信息相关
+ * @isDrawer 是否有抽屉
+ * @getUrl 列表请求接口地址
+ * @addUrl 新增接口地址
+ * @editUrl 编辑接口地址
+ * @deleteUrl 删除接口地址
+ * @author zhuwenfang
+ * @createtime 2020-07-28
+ */
+exports.conf = {
+    parentFolderPath: "E:/zash-cli/test",
+    author: "zhuwenfang",
+    title: "test文件",
+    isDrawer: true,
+    getUrl: "",
+    addUrl: "",
+    editUrl: "",
+    deleteUrl: ""
+}
