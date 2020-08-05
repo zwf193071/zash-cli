d32531b91ed1a30a9d27a88ae5ff1e7756ed00cf
zwf193071 <997131679@qq.com>
2020-08-05 17:31:01 +0800

    包安装后出现提示信息

--- postinstall.js
+++ postinstall.js
@@ -2,10 +2,8 @@ var env = process.env;
 
 var ADBLOCK = is(env.ADBLOCK);
 
-var BANNER = '\u001B[96mThank you for using zash-cli (\u001B[94m https://github.com/zwf193071/zash-cli \u001B[96m) to generate pages into VUE project!\u001B[0m\n\n';
-
-// var BANNER = '\u001B[96mThank you for using zash-cli (\u001B[94m https://github.com/zwf193071/zash-cli \u001B[96m) for generating pages into VUE project!\u001B[0m\n\n' +
-//              '\u001B[96mAlso, the author of zash-cli (\u001B[94m https://github.com/zwf193071 \u001B[96m) is looking for a great opportunity!)\u001B[0m\n';
+var BANNER = '\u001B[96mThank you for using zash-cli (\u001B[94m https://github.com/zwf193071/zash-cli \u001B[96m) for generating pages into VUE project!\u001B[0m\n\n' +
+             '\u001B[96mAlso, the author of zash-cli (\u001B[94m https://github.com/zwf193071 \u001B[96m) is looking for a great opportunity!)\u001B[0m\n';
 
 function is(it) {
   return !!it && it !== '0' && it !== 'false';
@@ -16,6 +14,4 @@ function isBannerRequired() {
   return true;
 }
 
-exports.showBanner = () => {
-  if (isBannerRequired()) console.log(BANNER);
-}
+if (isBannerRequired()) console.log(BANNER);
